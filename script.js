9fe542110d1526f6b427ac7319d1c872d91d5472
Marcelo Augusto <157186068+marceloab11@users.noreply.github.com>
2024-02-05 21:55:08 -0300

    + e - do carrinho, informações de preços no carrinho

--- script.js
+++ script.js
@@ -103,10 +103,15 @@ function updateCart() {
     if(cart.length > 0) {
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
+        let subTotal = 0;
+        let desconto = 0;
+        let total = 0;
 
         for(let i in cart) {
              let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
             let cartItem = c('.models .cart--item').cloneNode(true);
+            subTotal += pizzaItem.price * cart[i].qt;
+
 
             let pizzaSizeName;
             switch(cart[i].size) {
@@ -126,16 +131,30 @@ function updateCart() {
             cartItem.querySelector('img').src =pizzaItem.img;
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
             cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
-
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () => {
+                if(cart[i].qt > 1) {
+                    cart[i].qt--;
+                } else {
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', () => {
+                cart[i].qt++;
+                updateCart();
+            });
+            
             c('.cart').append(cartItem);
-
         }
+
+        desconto = subTotal * 0.1;
+        total = subTotal - desconto;
+
+        c('.subtotal span:last-child').innerHTML = `R$${subTotal.toFixed(2)}`
+        c('.desconto span:last-child').innerHTML = `R$${desconto.toFixed(2)}`
+        c('.total span:last-child').innerHTML = `R$${total.toFixed(2)}`
+        
     } else {
         c('aside').classList.remove('show');
     }
 }
-
-
-
-
-
